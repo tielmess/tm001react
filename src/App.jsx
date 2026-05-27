5e85382342a2207c48d7acbb77ce6487a39af97f
tielmess <trudy@trudymessingham.co.uk>
2026-05-27 14:57:19 +0100

    Add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>An h1 heading</h1>
-      <p>A paragraph of text</p>
-      <p>Another paragraph of text to test updates</p>
+      <h1 className="text-3xl font-bold text-blue-600">An h1 heading</h1>
+      <p>A paragraph of text.</p>
+      <p>Another paragraph of text to test updates.</p>
     </>
   );
 }
