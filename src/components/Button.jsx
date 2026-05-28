5099b27b199af463a14bb4dc28c2b38e38ef2841
tielmess <trudy@trudymessingham.co.uk>
2026-05-28 11:22:25 +0100

    Add button component (no href)

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button(props) {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+      {props.buttonText}
+    </button>
+  );
+}
+
+export default Button;
